37f4893c1df4400b353a06de942aae3ce6e975e0
JoanJuan10 <iijoanjuan10@gmail.com>
2020-12-30 09:12:48 +0100

    Fase 1

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom = "Joan";
+            string cognom1 = "Baza";
+            string cognom2 = "Tomas";
+
+            int dia = 30;
+            int mes = 12;
+            int any = 2020;
+
+            Console.WriteLine("{0} {1} {2}", cognom1, cognom2, nom);
+            Console.WriteLine("{0}/{1}/{2}", dia, mes, any);
+        }
+    }
+}
