3edeed32b69a943cad40cb4c35250e3d90920238
JoanJuan10 <iijoanjuan10@gmail.com>
2020-12-30 09:19:15 +0100

    Fase 2

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -6,16 +6,27 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            const int traspas = 1948;
+            int anysTraspas = 4;
+
             string nom = "Joan";
             string cognom1 = "Baza";
             string cognom2 = "Tomas";
 
-            int dia = 30;
-            int mes = 12;
-            int any = 2020;
+            int dia = 04;
+            int mes = 03;
+            int any = 2000;
 
             Console.WriteLine("{0} {1} {2}", cognom1, cognom2, nom);
             Console.WriteLine("{0}/{1}/{2}", dia, mes, any);
+
+            int anysTraspasats = 0;
+
+            for (int i = traspas; i < any; i += anysTraspas)
+            {
+                anysTraspasats++;
+            }
+            Console.WriteLine("Anys de traspas: {0}", anysTraspasats);
         }
     }
 }
